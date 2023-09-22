4269317ae3ff1125af38a91436d77b1c12699d74
Uncle Scientist <unclescientist@gmail.com>
2023-09-22 07:35:12 -0400

    add penup, pendown, and goto

--- src/draw.rs
+++ src/draw.rs
@@ -2,22 +2,32 @@ use crate::{Command, Turtle};
 
 impl Turtle {
     pub fn forward(&mut self, distance: f64) {
-        self.issue_command
-            .send(Command::Forward(distance))
-            .expect("graphics window no longer exists");
-        self.command_complete.recv().expect("main window died!");
+        self.do_command(Command::Forward(distance));
     }
 
     pub fn right(&mut self, rotation: f64) {
-        self.issue_command
-            .send(Command::Right(rotation))
-            .expect("graphics window no longer exists");
-        self.command_complete.recv().expect("main window died!");
+        self.do_command(Command::Right(rotation));
     }
 
     pub fn left(&mut self, rotation: f64) {
+        self.do_command(Command::Left(rotation));
+    }
+
+    pub fn penup(&mut self) {
+        self.do_command(Command::PenUp);
+    }
+
+    pub fn pendown(&mut self) {
+        self.do_command(Command::PenDown);
+    }
+
+    pub fn goto(&mut self, xpos: f64, ypos: f64) {
+        self.do_command(Command::GoTo(xpos, ypos));
+    }
+
+    fn do_command(&mut self, cmd: Command) {
         self.issue_command
-            .send(Command::Left(rotation))
+            .send(cmd)
             .expect("graphics window no longer exists");
         self.command_complete.recv().expect("main window died!");
     }
--- src/main.rs
+++ src/main.rs
@@ -2,38 +2,49 @@ use slowpoke::*;
 
 fn main() {
     Turtle::start(|turtle| {
-        spiky_fractal(turtle, 3, 243.);
+        turtle.forward(100.);
         turtle.right(90.);
-        square_fractal(turtle, 3, 243.);
+        turtle.penup();
+        turtle.forward(100.);
+        turtle.right(90.);
+        turtle.pendown();
+        turtle.forward(100.);
+
+        turtle.goto(0., 0.);
+        turtle.forward(100.);
+
+        // _spiky_fractal(turtle, 3, 243.);
+        // turtle.right(90.);
+        // _square_fractal(turtle, 3, 243.);
     });
 }
 
-fn spiky_fractal(turtle: &mut Turtle, order: usize, length: f64) {
+fn _spiky_fractal(turtle: &mut Turtle, order: usize, length: f64) {
     if order == 0 {
         turtle.forward(length);
     } else {
-        spiky_fractal(turtle, order - 1, length / 3.);
+        _spiky_fractal(turtle, order - 1, length / 3.);
         turtle.left(60.);
-        spiky_fractal(turtle, order - 1, length / 3.);
+        _spiky_fractal(turtle, order - 1, length / 3.);
         turtle.right(120.);
-        spiky_fractal(turtle, order - 1, length / 3.);
+        _spiky_fractal(turtle, order - 1, length / 3.);
         turtle.left(60.);
-        spiky_fractal(turtle, order - 1, length / 3.);
+        _spiky_fractal(turtle, order - 1, length / 3.);
     }
 }
 
-fn square_fractal(turtle: &mut Turtle, order: usize, length: f64) {
+fn _square_fractal(turtle: &mut Turtle, order: usize, length: f64) {
     if order == 0 {
         turtle.forward(length);
     } else {
-        square_fractal(turtle, order - 1, length / 3.);
+        _square_fractal(turtle, order - 1, length / 3.);
         turtle.left(90.);
-        square_fractal(turtle, order - 1, length / 3.);
+        _square_fractal(turtle, order - 1, length / 3.);
         turtle.right(90.);
-        square_fractal(turtle, order - 1, length / 3.);
+        _square_fractal(turtle, order - 1, length / 3.);
         turtle.right(90.);
-        square_fractal(turtle, order - 1, length / 3.);
+        _square_fractal(turtle, order - 1, length / 3.);
         turtle.left(90.);
-        square_fractal(turtle, order - 1, length / 3.);
+        _square_fractal(turtle, order - 1, length / 3.);
     }
 }
