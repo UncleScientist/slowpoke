31b863fad51a09eaa2d126f15bfe15fb7da626f5
Uncle Scientist <unclescientist@gmail.com>
2024-03-15 08:50:05 -0400

    the beginning of a fill example

---
+++ examples/star-shape.rs
@@ -0,0 +1,23 @@
+use slowpoke::*;
+
+fn main() {
+    TurtleArgs::default()
+        .with_size(400, 400)
+        .with_title("a line")
+        .run(|turtle| {
+            turtle.pencolor(1., 0., 0.);
+            // turtle.fillcolor(1., 1., 0.);
+            // turtle.begin_fill();
+            loop {
+                turtle.forward(200);
+                turtle.left(170);
+                let pos = turtle.pos();
+                let dist = ((pos[0] * pos[0] + pos[1] * pos[1]) as f64).sqrt();
+                if dist < 1.0 {
+                    break;
+                }
+            }
+
+            // turtle.end_fill();
+        });
+}
