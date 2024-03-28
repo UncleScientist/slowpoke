13f02fb382a7746b612bce587731388f56ca7cad
Uncle Scientist <unclescientist@gmail.com>
2024-03-28 07:57:24 -0400

    fix fractal due to turtle orientation change

--- examples/fractal.rs
+++ examples/fractal.rs
@@ -3,7 +3,7 @@ use slowpoke::*;
 fn main() {
     Turtle::new().run(|turtle| {
         turtle.penup();
-        turtle.goto(-243. / 2., -243. / 2.);
+        turtle.goto(-243. / 2., 243. / 2.);
         turtle.pendown();
         for _ in 0..4 {
             spiky_fractal(turtle, 3, 243.);
