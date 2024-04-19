ead01f8f93f5733e41adec9b702b06e67c0a82eb
Uncle Scientist <unclescientist@gmail.com>
2024-04-19 08:51:38 -0400

    real nested cube example (missed out from previous commit)

---
+++ examples/nested-cubes.rs
@@ -0,0 +1,17 @@
+use slowpoke::*;
+
+fn main() {
+    TurtleArgs::default()
+        .with_size(400, 400)
+        .with_title("nested cubes")
+        .run(|turtle| {
+            turtle.speed("fastest");
+            turtle.penwidth(2);
+            for i in 0..290 {
+                let i = i as f64;
+                turtle.pencolor(((i / 300.0 * 4.0) % 1.0, 1.0, 1.0));
+                turtle.forward(i);
+                turtle.right(60.0);
+            }
+        });
+}
