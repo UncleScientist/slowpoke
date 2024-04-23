9b7cb7fa97790fc326de3a95bafb2f507de97041
Uncle Scientist <unclescientist@gmail.com>
2024-04-23 07:12:25 -0400

    a few tweaks to make the code more visually appealing

--- examples/multi-turtle.rs
+++ examples/multi-turtle.rs
@@ -6,16 +6,27 @@ fn main() {
         .with_size(400, 400)
         .with_title("multiple turtles")
         .run(|turtle| {
+            turtle.penwidth(1);
+
             let mut t1 = turtle.hatch();
             t1.right(90);
+            t1.pencolor("red");
+            t1.penwidth(1);
+
             let mut t2 = turtle.hatch();
             t2.right(180);
+            t2.pencolor("green");
+            t2.penwidth(1);
+
             let mut t3 = turtle.hatch();
             t3.left(90);
-            let mut tlist = vec![turtle, &mut t1, &mut t2, &mut t3];
+            t3.pencolor("blue");
+            t3.penwidth(1);
+
+            let mut tlist = [turtle, &mut t1, &mut t2, &mut t3];
 
             for t in tlist.iter_mut() {
-                t.speed("fastest");
+                t.speed(10);
             }
 
             let mut rng = rand::thread_rng();
@@ -23,7 +34,6 @@ fn main() {
             loop {
                 for t in tlist.iter_mut() {
                     let dist: f64 = 10. + rng.gen::<f64>() * 20.;
-                    t.forward(dist);
                     let pos = t.pos();
                     if pos[0] > 200 || pos[0] < -200 || pos[1] > 200 || pos[1] < -200 {
                         let h = t.towards(0, 0);
@@ -32,6 +42,7 @@ fn main() {
                         let angle: f64 = rng.gen::<f64>() * 40. - 20.;
                         t.right(angle);
                     }
+                    t.forward(dist);
                 }
             }
         });
