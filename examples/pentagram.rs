73b46d5718e645304778a464ecba557b3af57cc0
Uncle Scientist <unclescientist@gmail.com>
2024-05-03 08:41:04 -0400

    a pentagram to demonstrate undoing a fill operation

---
+++ examples/pentagram.rs
@@ -0,0 +1,21 @@
+use slowpoke::*;
+
+fn main() {
+    TurtleArgs::default()
+        .with_size(400, 400)
+        .with_title("a pentagram")
+        .run(|turtle| {
+            turtle.fillcolor("green");
+            turtle.begin_fill();
+            for _ in 0..5 {
+                turtle.forward(100);
+                turtle.right(180 - 36);
+            }
+            turtle.end_fill();
+            turtle.speed(1);
+            turtle.goto(-200, -200);
+            while turtle.undobufferentries() > 0 {
+                turtle.undo();
+            }
+        });
+}
