04571f6d1c946a113fbbd999083b273e35dd94e2
Uncle Scientist <unclescientist@gmail.com>
2024-05-10 08:08:37 -0400

    drawing some circle stuff

---
+++ examples/basic_circle.rs
@@ -0,0 +1,16 @@
+use slowpoke::*;
+
+fn main() {
+    TurtleArgs::default()
+        .with_size(400, 400)
+        .with_title("a basic circle")
+        .run(|turtle| {
+            turtle.forward(100);
+            turtle.right(90);
+            turtle.speed(1);
+            turtle.pencolor("green");
+            turtle.circle(155, 180, 250);
+            turtle.pencolor("grey");
+            turtle.circle(50, 270, 20);
+        });
+}
