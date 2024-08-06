d1ae828f4e0ccc90c871aa68b991f7364017f7d4
Uncle Scientist <unclescientist@gmail.com>
2024-08-06 08:03:02 -0400

    missing call to circle

--- examples/doraemon.rs
+++ examples/doraemon.rs
@@ -31,6 +31,7 @@ fn main() {
                 turtle.begin_fill();
                 turtle.setheading(0);
                 turtle.forward(200);
+                turtle.circle(-5).with_extent(90);
                 turtle.forward(10);
                 turtle.circle(-5).with_extent(90);
                 turtle.forward(207);
