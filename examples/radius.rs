ab2cbe498ae9a476157333fb9205b2306b711036
Uncle Scientist <unclescientist@gmail.com>
2024-08-06 08:03:46 -0400

    stop between each command

--- examples/radius.rs
+++ examples/radius.rs
@@ -5,16 +5,42 @@ fn main() {
         .with_size(600, 400)
         .with_title("a corner radius")
         .run(|turtle| {
+            let mut s = String::new();
+
             turtle.speed(1);
             turtle.fillcolor("#e70010");
             turtle.begin_fill();
             turtle.setheading(0);
+            println!("forward 200");
+            let _ = std::io::stdin().read_line(&mut s);
             turtle.forward(200);
-            turtle.forward(10);
+
+            println!("circle -5 90");
+            let _ = std::io::stdin().read_line(&mut s);
             turtle.circle(-5).with_extent(90);
+
+            println!("forward 10");
+            let _ = std::io::stdin().read_line(&mut s);
+            turtle.forward(10);
+
+            println!("circle -5 90");
+            let _ = std::io::stdin().read_line(&mut s);
+            turtle.circle(-5).with_extent(90);
+
+            println!("forward 207");
+            let _ = std::io::stdin().read_line(&mut s);
             turtle.forward(207);
+
+            println!("circle -5 90");
+            let _ = std::io::stdin().read_line(&mut s);
             turtle.circle(-5).with_extent(90);
+
+            println!("forward 10");
+            let _ = std::io::stdin().read_line(&mut s);
             turtle.forward(10);
+
+            println!("circle -5 90");
+            let _ = std::io::stdin().read_line(&mut s);
             turtle.circle(-5).with_extent(90);
             /*
             turtle.forward(100);
