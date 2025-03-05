1a94118f71a3a79f6c47ec4618192715f040d5f2
Uncle Scientist <unclescientist@gmail.com>
2025-03-05 07:58:02 -0500

    started real work on egui UI

--- crates/slowpoke-egui/src/lib.rs
+++ crates/slowpoke-egui/src/lib.rs
@@ -1,12 +1,148 @@
-use slowpoke::{SlowpokeLib, TurtleUserInterface};
+use std::collections::HashMap;
+
+use egui::{pos2, vec2, Color32, Painter, Pos2, Rect, Stroke};
+use slowpoke::{
+    DrawCommand, Handler, IndividualTurtle, LineInfo, PopupID, SlowpokeLib, TurtleColor, TurtleGui,
+    TurtleID, TurtleTask, TurtleUI, TurtleUserInterface,
+};
 
 pub type Slowpoke = SlowpokeLib<EguiFramework>;
+pub type Turtle = slowpoke::Turtle;
 
 #[derive(Debug)]
 pub struct EguiFramework;
 
-impl TurtleUserInterface for EguiFramework {
-    fn start(_flags: slowpoke::TurtleFlags) {
+#[derive(Debug, Default)]
+struct EguiInternal;
+
+impl TurtleUI for EguiInternal {
+    fn generate_popup(&mut self, popupdata: &slowpoke::PopupData) -> PopupID {
         todo!()
     }
+
+    fn resize(&mut self, width: isize, height: isize) {
+        // TODO
+    }
+
+    fn set_bg_color(&mut self, bgcolor: TurtleColor) {
+        // TODO
+    }
+}
+
+#[derive(Debug, Default)]
+struct EguiUI {
+    drawing: Vec<EguiCmd>,
+}
+
+#[derive(Debug)]
+enum EguiCmd {
+    Line(Pos2, Pos2),
+}
+
+impl EguiUI {
+    fn convert(&mut self, pct: f32, cmds: &[DrawCommand], turtle: &IndividualTurtle<EguiUI>) {
+        let mut iter = cmds.iter().peekable();
+        let mut tpos = pos2(0.0, 0.0);
+        let mut trot = 0f32;
+
+        while let Some(element) = iter.next() {
+            let last_element = iter.peek().is_none() && pct < 1.;
+
+            match element {
+                DrawCommand::Line(line) => {
+                    let (start, end) = Self::start_and_end(last_element, pct, line);
+                    tpos = end;
+                    self.drawing.push(EguiCmd::Line(start, end));
+                }
+                _ => {}
+            }
+        }
+    }
+
+    fn draw(&self, painter: &Painter, cur_size: &Rect) {
+        let center = vec2(cur_size.max.x / 2.0, cur_size.max.y / 2.0);
+
+        let stroke = Stroke::new(0.25, Color32::WHITE);
+        for cmd in &self.drawing {
+            match cmd {
+                EguiCmd::Line(start, end) => {
+                    painter.line(vec![*start + center, *end + center], stroke);
+                }
+            }
+        }
+    }
+
+    fn start_and_end(last_element: bool, pct: f32, line: &LineInfo) -> (Pos2, Pos2) {
+        (
+            pos2(line.begin.x as f32, line.begin.y as f32),
+            if last_element {
+                let end_x = line.begin.x as f32 + (line.end.x - line.begin.x) as f32 * pct;
+                let end_y = line.begin.y as f32 + (line.end.y - line.begin.y) as f32 * pct;
+                pos2(end_x, end_y)
+            } else {
+                pos2(line.end.x as f32, line.end.y as f32)
+            },
+        )
+    }
+}
+
+impl TurtleUserInterface for EguiFramework {
+    fn start(mut flags: slowpoke::TurtleFlags) {
+        let func = flags.start_func.take();
+        let title = flags.title.clone();
+        let mut tt = TurtleTask::new(&mut flags);
+        tt.run_turtle(func.unwrap());
+
+        let mut handler = Handler::<EguiUI, EguiInternal> {
+            last_id: TurtleID::default(),
+            turtle: HashMap::new(),
+            title: format!(" {title} "),
+            popups: HashMap::new(),
+            screen: EguiInternal {
+                ..EguiInternal::default()
+            },
+        };
+        let _ = handler.new_turtle();
+
+        let options = eframe::NativeOptions {
+            viewport: egui::ViewportBuilder::default()
+                .with_inner_size([flags.size[0], flags.size[1]]),
+            ..Default::default()
+        };
+
+        eframe::run_simple_native(title.as_str(), options, move |ctx, _frame| {
+            tt.tick(&mut handler);
+
+            if update_turtles(&mut handler, &mut tt) {
+                egui::CentralPanel::default().show(ctx, |ui| {
+                    let cur_size = ctx.screen_rect();
+                    let painter = ui.painter();
+
+                    for turtle in handler.turtle.values() {
+                        let ui = turtle.ui.borrow();
+                        ui.draw(painter, &cur_size);
+                    }
+                });
+            }
+        })
+        .expect("failed to start turtle");
+    }
+}
+
+fn update_turtles(handler: &mut Handler<EguiUI, EguiInternal>, tt: &mut TurtleTask) -> bool {
+    let mut done = true;
+
+    for (tid, turtle) in handler.turtle.iter_mut() {
+        let (pct, prog) = tt.progress(*tid);
+        if turtle.has_new_cmd {
+            done = false;
+            let mut ui = turtle.ui.borrow_mut();
+            ui.convert(pct, &turtle.cmds, &turtle);
+            if prog.is_done(pct) {
+                turtle.has_new_cmd = false;
+            }
+        }
+    }
+
+    !done
 }
